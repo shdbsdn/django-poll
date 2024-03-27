7eef4d0b2df8ac877b7334bb99512fa0ba933856
Roh Yunwoo <woo07027@naver.com>
2024-03-27 15:27:49 +0900

    step1-urls,views

--- poll/polls/urls.py
+++ poll/polls/urls.py
@@ -1,8 +1,7 @@
-from django.urls import include,path
-from django.contib import admin
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
 ]
